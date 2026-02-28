7d546725069f43bef613787efcd79bf7b18380dc
Musheer0 <musheeran165@gmail.com>
2026-02-28 20:04:27 +0530

    fix cors3

--- src/lib/set-cors-header.ts
+++ src/lib/set-cors-header.ts
@@ -13,7 +13,7 @@ export function corsHeaders(origin: string | null) {
       const clean = cleanOrigin(origin);
 
   return {
-    "Access-Control-Allow-Origin": clean ?? "",
+    "Access-Control-Allow-Origin": clean ,
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Allow-Credentials": "true",
